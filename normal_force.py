bb91323460fc059712642b58e78502c7f4711939
Hans Engstad <hans.engstad@outlook.com>
2020-09-22 11:05:32 +0200

    Normal force added

---
+++ normal_force.py
@@ -0,0 +1,49 @@
+import numpy as np
+import utils
+import velocity
+import curvature
+import angle
+
+"""
+Calculate the normal force in each of the 1400 steps (Normalkraft)
+"""
+
+
+def calculate():
+
+    # Define some constants
+    g = 9.81  # Gravity constant
+    c = 1.5  # Mass distribution constant
+    m = 2  # Mass of ball (TODO: Insert correct mass)
+
+    v = velocity.calculate()
+    k = curvature.calculate()
+    beta = angle.calculate()
+
+    n = []
+    for i in range(len(v)):
+        # Calculate centripetal acceleration in this point (Sentripetalakselerasjon)
+        a = v[i] ** 2 * k[i]
+
+        # Calucalte normal force in this point
+        current_n = m * (g * np.cos(beta[i]) + a)
+
+        # Append value to the results
+        n.append(current_n)
+
+    return n
+
+
+def main():
+    """Method to execute when script is invoked"""
+
+    # Retrieve normal force list
+    n = calculate()
+
+    # Plot the results
+    utils.plot(n, y_min=0, y_max=50, y_unit="N")
+
+
+# Snippet to run main function when script is executed
+if __name__ == "__main__":
+    main()
